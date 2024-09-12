032b799316a43c59d89b02ecc804209a9799ec61
MrPanther213 <117368020+MrPanther213@users.noreply.github.com>
2024-09-12 09:53:09 -0400

    q

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("The fog is coming")
+import zechariah_kurz
+import lastname_firstname
+def introduce_yourself():
+    zechariah_kurz.say_hello()
+    lastname_firstname.say_hello()
+
+introduce_yourself()
