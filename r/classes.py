5eda9b7ad8d0c9ac950a3f80f06494fc20a328a5
MrPanther213 <117368020+MrPanther213@users.noreply.github.com>
2024-09-17 10:06:10 -0400

    q

---
+++ r/classes.py
@@ -0,0 +1,13 @@
+class Dog:
+
+    Species = "Healer" 
+
+
+    def __init__(self, name, age, breed, fur_type, sheds): 
+        self.name= name
+        self.age = age
+        self.breed=breed
+        self.fur_type=fur_type
+        self.sheds=sheds               #insurt atrubutes for all
+        description:
+        
